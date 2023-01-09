755a9fdd48b8e3d7034cc2150298aa1fe88c7974
SuperPrintf <2211613889@qq.com>
2023-01-10 04:19:01 +0800

    build

--- 1.build/hello_cargo/src/main.rs
+++ 1.build/hello_cargo/src/main.rs
@@ -1,4 +1,5 @@
-fn main() {
-    println!("Hello, world!");
-    print!("Hello, cargo!\n");
-}
+//fn声明一个函数,这里是一个名为main,没有入参和返回值的函数
+fn main() { //main()是Rust的入口函数
+    println!("Hello, world!"); //这里并非一个普通函数,而是名为println!的宏(格式化输出并换行)
+    print!("Hello, cargo!\n"); //Rust中所有以!结尾的都意味着是宏而非函数
+}//{}用以标记函数体,Rust要求所有函数体都要被{}阔起
